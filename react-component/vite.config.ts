66f53df7f16c481260c15898aa1015cd3ee1ee3e
nkgrnkgr <beefupme@gmail.com>
2024-12-04 16:58:38 +0900

    update-vite-config

--- react-component/vite.config.ts
+++ react-component/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'react-component-lib',
+      fileName: 'react-component-lib',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        // globals: {
+        //   react: 'React',
+        //   'react-dom': 'ReactDOM',
+        // },
+      },
+    },
+  },
+});
