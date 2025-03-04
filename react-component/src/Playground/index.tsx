c459a520af3d89fa10b6ce5eb3e4314b04af25c3
nkgrnkgr <beefupme@gmail.com>
2025-03-04 11:33:50 +0900

    tailwindをいれる

--- react-component/src/Playground/index.tsx
+++ react-component/src/Playground/index.tsx
@@ -1,7 +1,7 @@
 export function Playground() {
   return (
-    <div className="text-xs font-bold text-sky-500">
-      <h1>Playground</h1>
+    <div>
+      <h1 className="text-gray-700">Playground</h1>
       <p>Playground content goes here</p>
     </div>
   );
--- react-component/src/index.tsx
+++ react-component/src/index.tsx
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 import { Button } from "./Button";
 import { EventListener } from "./EventListener";
 import { Playground } from "./Playground";
+import styles from "./index.module.css";
 
 const root = document.getElementById("react-root");
 if (root) {
   createRoot(root).render(
     <StrictMode>
-      <div style={{ margin: "100px 20px", backgroundColor: "skyblue" }}>
+      <div
+        className={styles.root}
+        style={{ margin: "100px 20px", backgroundColor: "skyblue" }}
+      >
         <h1>This is React Component</h1>
         <Button />
         <EventListener />
